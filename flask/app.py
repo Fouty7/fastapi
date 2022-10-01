f0aa6f1b2fe8ecc8383f1e22f9998c140129ab15
Aminu Ndakun <Amin4712@gmail.com>
2022-10-01 15:15:12 +0100

    setting up sqlalchemy db

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) 
+
+@app.get("/")
+def home():
+    return "Hello, World"
